45fa5a87c1b8fdef65b6c52f5b8e8365345693bd
Plinio Naves <pliniopjn@hotmail.com>
2019-01-25 18:19:19 -0200

    Mapeando o State via "mapState"

--- src/components/Contador.vue
+++ src/components/Contador.vue
@@ -2,6 +2,8 @@
     <div>
 
         <h2 class="font-weight-light">Contador: {{ contador }}</h2>
+        <h4 class="font-weight-light">Contador Alias: {{ contadorAlias }}</h4>
+        <h4 class="font-weight-light">Contador Multiplicado: {{ contadorMultiplicado }}</h4>
         <button class="btn btn-lg btn-danger mr-1" @click="decrementar">-</button>
         <button class="btn btn-lg btn-success" @click="incrementar">+</button>
 
@@ -9,12 +11,24 @@
 </template>
 
 <script>
+
+import { mapState } from 'vuex'
+
 export default {
-    computed: {
-        contador() {
-            return this.$store.state.contador
+    data() {
+        return {
+            contadorLocal: 2
         }
     },
+    // computed: mapState(['contador']),
+    computed: mapState({
+        // contador: state => state.contador
+        contador: 'contador',
+        contadorAlias: 'contador',
+        contadorMultiplicado(state) {
+             return state.contador * this.contadorLocal
+        }
+    }),
     methods: {
         decrementar() {
             this.$store.state.contador--
