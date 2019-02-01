f7179f0c687051b914d2757aecf14bc4c23f52d9
Plinio Naves <pliniopjn@hotmail.com>
2019-02-01 13:25:59 -0200

    Bônus: implementando Mutations do Módulo Tarefas

--- src/resources/tarefas/_store/mutations.js
+++ src/resources/tarefas/_store/mutations.js
@@ -1,8 +1,22 @@
 import {
+    CRIAR_TAREFA,
+    EDITAR_TAREFA,
+    DELETAR_TAREFA,
     LISTAR_TAREFAS
 } from './mutation-types'
 
 export default {
+    [CRIAR_TAREFA]: (state, { tarefa }) => {
+        state.tarefas.push(tarefa)
+    },
+    [EDITAR_TAREFA]: (state, { tarefa }) => {
+        const indice = state.tarefas.findIndex(t => t.id === tarefa.id)
+        state.tarefas.splice(indice, 1, tarefa)
+    },
+    [DELETAR_TAREFA]: (state, { tarefa }) => {
+        const indice = state.tarefas.findIndex(t => t.id === tarefa.id)
+        state.tarefas.splice(indice, 1)
+    },
     [LISTAR_TAREFAS]: (state, { tarefas }) => {
         state.tarefas = tarefas
     }
